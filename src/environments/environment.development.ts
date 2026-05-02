97f212dbbdcbd5a6f0eaa5251a1adf4d812860dc
NiNejah <almawlanejeh@gmail.com>
2026-05-02 12:27:06 +0200

    fix: remove rawgApi

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 // Get a free RAWG API key at https://rawg.io/apidocs and paste it below.
 export const environment = {
   production: false,
-  rawgApiKey: '96682915c9494c708354b7a62a3cf6df',
+  rawgApiKey: '',
   rawgBaseUrl: 'https://api.rawg.io/api',
 };
