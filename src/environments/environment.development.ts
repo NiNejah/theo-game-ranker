3b8c5d235b48908e682575479745bb8138bc8575
NiNejah <almawlanejeh@gmail.com>
2026-05-02 11:39:07 +0200

    fix: rawgApiKey in environment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 // Get a free RAWG API key at https://rawg.io/apidocs and paste it below.
 export const environment = {
   production: false,
-  rawgApiKey: 'YOUR_RAWG_API_KEY',
+  rawgApiKey: '96682915c9494c708354b7a62a3cf6df',
   rawgBaseUrl: 'https://api.rawg.io/api',
 };
